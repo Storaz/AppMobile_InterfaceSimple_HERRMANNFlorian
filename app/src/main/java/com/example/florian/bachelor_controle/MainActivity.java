193582ad8f96382a79c5ae85c9cc0da4a8abdd53
Florian <f.herrmann@ludus-academie.com>
2018-10-26 16:53:22 +0200

    no message

---
+++ app/src/main/java/com/example/florian/bachelor_controle/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.florian.bachelor_controle;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import java.text.DateFormat;
+import java.util.Date;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Log.i("Bienvenue dans l'application: ","@string/app_name");
+
+        long date = System.currentTimeMillis();
+
+        TextView textView=  (TextView)findViewById(R.id.textView);
+        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+        textView.setText(currentDateTimeString);
+
+        EditText Recup_Prenom;
+        TextView Recup_Nom;
+
+        Recup_Prenom=(EditText) findViewById(R.id.editTextPrenom);
+        Recup_Nom=(EditText) findViewById(R.id.editTextNom);
+
+
+
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
